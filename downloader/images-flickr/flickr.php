eb2aa938bd7b0a844db853a73afb023633ffe5ea
annafatsevych <anna.fatsevych@gmail.com>
2015-06-18 16:11:12 -0400

    Added phpFlickr.php

--- downloader/images-flickr/flickr.php
+++ downloader/images-flickr/flickr.php
@@ -1,28 +1,38 @@
 <?php
-// Program that will download Flickr Images and Store them in your database based on Date
-// By Anna Fatsevych
-// June 11, 2015
-// Uses phpFlickr Flickr API for PHP
-$date = NULL;
-foreach ($argv as $arg) {
-    $e=explode("=",$arg);
-    if(count($e)==2){
-        $_GET[$e[0]]=$e[1];
 
-    }else
-        $_GET[]=$e[0];        
-}
-$date = $_GET[1];
-
+// Flickr PHP Image Downloader
+// By Anna Fatsevych uses phpFlickr.php
+// Downloads Commons images for the range of provided dates. 
 require_once("phpFlickr.php");
 
+$date = NULL;
 
-// Database stats
-$servername = "localhost";
-$username = "anna";
-$password = "password";
-$dbname = "hashes";
+foreach ($argv as $arg) {
+  $e=explode("=",$arg);
+  if(count($e)==2){
+    $_GET[$e[0]]=$e[1];
+    $_GET[$e[1]]=$e[2];
+  }else
+    $_GET[]=$e[0];        
+  }
 
+$date = $_GET[1];
+$date2 = $_GET[2];
+
+if(empty($date2)){
+  echo "\nNO DATE 2\n";
+  $date2 = $date;
+}
+
+
+//Create SQLite Database
+class MyDB extends SQLite3
+{
+  function __construct($date, $date2)
+  {
+   $this->open($date."-".$date2.'_flickrdatabase.db');
+ }
+}
 
 $license1 = "CC-BY-NC-SA-2.0";
 $license2 = "CC-BY-NC-2.0";
@@ -31,96 +41,161 @@ $license4 = "CC-BY-2.0";
 $license5 = "CC-BY-SA-2.0";
 $license6 = "CC-BY-ND-2.0";
 $license7 = "CC-Zero";
+
 $errorcount=0;
+$errorname = "";
+// Files
+$myfile = fopen($date."-".$date2."_flickrdownload.txt", "w") or die("Unable to open file!");
+$errorfile = fopen($date."-".$date2."_flickrerrors.txt", "w") or die("Unable to open errorfile file!");
+
+//Flickr File
+$f = new phpFlickr("dd5266efb4a0e67238c32f8b8cfa2f92");
+$dir = $date."-".$date2."_pics";
+mkdir($dir);
 
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+while (strtotime($date) <= strtotime($date2)) {
+  echo "$date\n";
 
-$f = new phpFlickr("API KEY");
+  try{
+    $photos = $f->photos_search(array("min_upload_date"=>$date,"max_upload_date"=>$date,"per_page"=>"500","license"=>"1,2,3,4,5,6", "extras"=>"url_o,owner_name, license"));
 
-$photos = $f->photos_search(array("min_upload_date"=>$date, "max_upload_date"=>$date,"per_page"=>"500","license"=>"3", "extras"=>"url_o,owner_name, license"));
+    echo "\nTOTAL: ".$photos['total'];
+    echo "\nPAGES: ".$photos['pages'];
+    $pages = $photos['pages'];
+    
 
-$counter = 0;
-foreach ($photos['photo'] as $photo) {
+    for($page=1; $page <= $pages; $page++){
+      $counter = 0;
+      echo "\nFETCHING FOR PAGE: ".$page;
+      $photos = $f->photos_search(array("min_upload_date"=>$date, "max_upload_date"=>$date,"page"=>$page,"per_page"=>"500","license"=>"1,2,3,4,5,6", "extras"=>"url_o,owner_name, license"));
+      
+      foreach ($photos['photo'] as $photo) {
+        $filename = $dir."/".$photo['owner'].$photo['id'];
+        $url = $photo['url_o'];
 
-//$owner = $f->photos_getInfo($photo['id']);
-$filename = 'pics/'. $photo['owner'].$photo['id'];
-$url = $photo['url_o'];
+        try{
+          file_put_contents($filename, file_get_contents($url));
+        }catch (Exception $e){
+          echo "\n\n DOWNLOAD ERROR: ".$e."\n\n";
+        }
+        try{
+          $str = 'convert '.$filename.' -resize 200 '.$filename;
+          $thumb = exec($str);
+        }
+        catch (Exception $e){
+          echo "\n\n RESIZE ERROR!  ".$e."\n\n";
+        }
+        try{
+          $hash = exec('./phash '.$filename);
 
-file_put_contents($filename, file_get_contents($url));
+        }catch (Exception $e) {
+          echo "\n\n HASH ERROR: ".$e."\n\n";
+        }
+        try{
+          $mhash = exec('./phashmh '.$filename);
+        }
+        catch (Exception $e){
+          echo "\n\n MPH HASH EXCEPTION!!! ".$e."\n\n";
+        }
 
-try{
-    $hash = exec('./phash '.$filename);
-    echo $hash;
-}catch (Exception $e) {
-    echo "\n\n HASH ERROR: ".$e."\n\n";
-}
-try{
-    $mhash = exec('./phashmh '.$filename);
-    echo $mhash;
-}
-catch (Exception $e){
-    echo "\n\n MPH HASH EXCEPTION!!! ".$e."\n\n";
-}
+        $license = $photo['license'];
+        switch ($photo['license']) {
+          case "1":
+          $license = $license1;
+          break;
+          case "2":
+          $license = $license2;
+          break;
+          case "3":
+          $license = $license3;
+          break;
+          case "4":
+          $license = $license4;
+          break;
+          case "5":
+          $license = $license5;
+          break;
+          case "6":
+          $license = $license6;
+          break;
+          default:
+          echo $license;
+        }
+        $photograph = file_get_contents($filename);
+        $authorname = $photo['ownername'];
+        $title = $photo['title'];
 
-$license = $photo['license'];
-switch ($photo['license']) {
-    case "1":
-        $license = $license1;
-        break;
-    case "2":
-        $license = $license2;
-        break;
-    case "3":
-        $license = $license3;
-        break;
-    case "4":
-        $license = $license4;
-        break;
-    case "5":
-        $license = $license5;
-        break;
-    case "6":
-        $license = $license6;
-        break;
-    default:
-        echo $license;
-}
+        if(empty($hash) || empty($license) || empty($photograph) || empty($title)
+          || empty($authorname) || empty($url) || empty($mhash)){
 
-try{
-    $str = 'convert '.$filename.' -resize 200 '.$filename;
-    $thumb = exec($str);
-    echo "\n\n Resizing ***  \n";
-}
-catch (Exception $e){
-    echo "\n\n RESIZE ERROR!  ".$e."\n\n";
-}
+          $errorname = (empty($photos))? "No RESPONSE" : "";
+ 
+          $errorname = "page: ".$page." date: ".$date."\n";
+          fwrite($errorfile, $errorname);
+          $errorcount++;    
+      }else{
 
-$counter++;
-//echo "\n ** COUNTER: ".$counter;
+        $db = new MyDB($date, $date2);
+        if(!$db){
+          echo $db->lastErrorMsg();
+        } else {
+          echo "Opened database successfully\n";
+        }
 
-$photograph = file_get_contents($filename);
-$authorname = $photo['ownername'];
-$title = $photo['title'];
+        $sql =<<<EOF
+        CREATE TABLE IF NOT EXISTS IMG (
+          id INTEGER PRIMARY KEY, 
+          phash VARCHAR(22) NOT NULL,
+          license VARCHAR(45) NOT NULL, 
+          image BLOB,
+          imagename VARCHAR(1024),
+          url VARCHAR(1024),
+          local VARCHAR(1024),
+          dateuploaded DATE,
+          timestamp DATE,
+          mhash VARCHAR(75));
+EOF;
 
- if(empty($hash) || empty($license) || empty($photograph) || empty($title)
-            || empty($authorname) || empty($url) || empty($mhash)){
+        $ret = $db->exec($sql);
+        if(!$ret){
+          echo $db->lastErrorMsg();
+        } else {
+          echo "Table created successfully\n";
+        }
 
-            // Write to File
-   
-        } /*else {
-            try{
-                // MYSql Insert Statement
-            }
-            catch (Exception $e){
-                echo "\n\nDB ERROR!! ".$e;         
-            }
-                } */
+                // Prepare INSERT statement to SQLite3 file db
+        $insert = "INSERT INTO IMG (phash, license, image,imagename,  url, mhash, local, dateuploaded, timestamp) 
+        VALUES (:phash, :license, :photograph, :imagename, :url, :mhash, :filename, :dateuploaded, :timestamp)";
 
-}
+        $stmt = $db->prepare($insert);
+        $sqlite_timestamp = date(DATE_RFC3339);
+
+        // Bind parameters to statement variables
+        $stmt->bindParam(':phash', $hash);
+        $stmt->bindParam(':license', $license);
+        $stmt->bindParam(':imagename', $title);
+        $stmt->bindParam(':photograph', $photograph);
+        $stmt->bindParam(':url', $url);
+        $stmt->bindParam(':mhash', $mhash);
+        $stmt->bindParam(':filename', $filename);
+        $stmt->bindParam(':dateuploaded', $date);
+        $stmt->bindParam(':timestamp', $sqlite_timestamp);
+
+        // Execute statement
+        $stmt->execute();
+    }
+
+    $counter++;
+    } // close foreach photo
+  }
+
+  } catch (Exception $e){
+    echo "\n Connection error\n";
+  }
+  $db->close();
+  fclose($myfile);
+  fclose($errorfile);
+  $date = date ("Y-m-d", strtotime("+1 day", strtotime($date)));
+ }
 ?>
