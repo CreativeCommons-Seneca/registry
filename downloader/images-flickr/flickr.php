5dbd7eb2eec0e1b87e554ba19b8386d2332def16
annafatsevych <anna.fatsevych@gmail.com>
2015-07-02 17:30:15 -0400

    interrupt testing for time intervals added

--- downloader/images-flickr/flickr.php
+++ downloader/images-flickr/flickr.php
@@ -3,11 +3,12 @@
 // Flickr API 
 require_once("phpflickr-master/phpFlickr.php");
 //Flickr File
-
+ini_set("memory_limit","600M");
 date_default_timezone_set('Greenwich');
 
 $date = NULL;
 $date2 = NULL;
+
 foreach ($argv as $arg) {
   $e=explode("=",$arg);
   if(count($e)==2){
@@ -41,6 +42,9 @@ $errorcount=0;
 $errorname = "";
 $del = '\',\'';
 
+$duplicate = false;
+$interrupinterval = 1;
+
 
 
 
@@ -50,9 +54,17 @@ while (strtotime($date) <= strtotime($date2)) {
   if (file_exists($date."_flickrdownload.txt")) {
   $myfile = fopen($date."_flickrdownload.txt", 'a') or die("Unable to open errorfile file!");
   echo "\n\nFILE EXISTS!";
+
+  $lastline= read_last_line($date."_flickrdownload.txt");
+  $interruptedfile = (explode($del,$lastline));
+  print_r($interruptedfile);
+  $interruptdate = $interruptedfile[10];
+  $interrupinterval = $interruptedfile[11];
+  $startpage = $interruptedfile[12];
   $interrupt = true;
    $f = fopen($date."_flickrdownload.txt", 'rb');
-    $lines = 0;
+    
+    /*$lines = 0;
     while (!feof($f)) {
         $lines += substr_count(fread($f, 8192), "\n");
     }
@@ -64,7 +76,8 @@ while (strtotime($date) <= strtotime($date2)) {
       $lines=$lines-500;
       echo "\nLINES : ".$lines;
     }
-
+*/
+    $lines = $interruptedfile[13];
     echo "\n\nINTER PAGE: ".$startpage;
 
 } else {
@@ -76,153 +89,221 @@ if (file_exists($date."_flickrerrors.txt")) {
 } else {
   $errorfile = fopen($date."_flickrerrors.txt", "w") or die("Unable to open errorfile file!");
 }
-
+if (file_exists($date."_duplicates.txt")) {
+  $duplicates = fopen($date."_duplicates.txt", "a") or die("Unable to open errorfile file!");
+} else {
+  $duplicates = fopen($date."_duplicates.txt", "w") or die("Unable to open errorfile file!");
+}
 
 
 
 $dir = $date."_pics";
 mkdir($dir);
-
+$beforet = new DateTime($date);
   try{
-  $beforet = ($date." 00:00:00");
-  $beforet = new DateTime($beforet);
-  $before =$beforet->format('Y-m-d H:i:sP');
 
-  echo "\n\nBEFORE TIME: ".$beforet->format('Y-m-d H:i:sP');
+    for($x = $interrupinterval; $x <= 48; $x++){
+      //$beforet = ($date." 00:00:00");
 
-  $aftert = ($date." 23:59:59");
-  $aftert = new DateTime($aftert);
-  $after = $aftert->format('Y-m-d H:i:sP');
-
-  $start = microtime(true);
-  $f = new phpFlickr("dd5266efb4a0e67238c32f8b8cfa2f92");
-  $photos = $f->photos_search(array("max_upload_date"=>$after,"min_upload_date"=>$before,"per_page"=>"500","license"=>"1,2,3,4,5,6,7","extras"=>"url_o,owner_name, license, date_upload"));
-  echo "\nTOTAL ************************** : ".$photos['total'];
-  echo "\nPAGES ************************** : ".$photos['pages'];
-  $pages = $photos['pages'];
-  echo "\n\nFOR DATE ********************* : ".$date;
-  //print_r($photos);
-
-  for($page=$startpage; $page <= $pages; $page++){
-    $counter = 0;
-    echo "\nFETCHING FOR PAGE: ".$page;
-    $photos = $f->photos_search(array("max_upload_date"=>$after,"min_upload_date"=>$before,"per_page"=>"500","license"=>"1,2,3,4,5,6,7","page"=>$page,"per_page"=>"500","extras"=>"url_o,owner_name, license, date_upload"));
+      if($x == 48){
+        $interval = new DateInterval('PT29M59S');
+      } else{
 
 
-    $i = 1;
+      $beforeinterval = new DateInterval('PT'.($x-1)*(30).'M');
+      $beforet->add($beforeinterval);
+      $interval = new DateInterval('PT30M');
+      }
+      
+      $before =$beforet->format('Y-m-d H:i:sP');
 
-    foreach ($photos['photo'] as $photo) {
+      echo "\n\nBEFORE TIME: ".$before;
 
-      if($interrupt == false || ($interrupt == true && $lines < $i)){
+      
+
+      $aftert = new DateTime($before);
+      $aftert->add($interval);
+      $after = $aftert->format('Y-m-d H:i:sP');
+
+      echo "\n\nAFTER  TIME: ".$after;
+      echo "\n\n INTERVAL ".$x;
+
+      $start = microtime(true);
+      if (($interrupt == false )|| (($interrupt == true) && (strcmp($date, $interruptdate) === 0) && ($x == $interrupinterval))){
+      if($interrupt === false){
+        $starpage = 1;
+      } 
   
+      $f = new phpFlickr("dd5266efb4a0e67238c32f8b8cfa2f92");
+      $photos = $f->photos_search(array("max_upload_date"=>$after,"min_upload_date"=>$before,"per_page"=>"500","license"=>"1,2,3,4,5,6,7","extras"=>"url_o,owner_name, license, date_upload"));
+      echo "\nTOTAL ************************** : ".$photos['total'];
+      echo "\nPAGES ************************** : ".$photos['pages'];
+      $pages = $photos['pages'];
+      echo "\n\nFOR DATE ********************* : ".$date;
+      //print_r($photos);
+      echo "\n\n STARTPAGE ".$startpage;
 
-        $url = $photo['url_o'];
-        $namefile = explode("/",$url);
-        $filename = $dir."/".$namefile[4];
-        $crediturl = "https://flickr.com/photos/".$photo['owner']."/".$photo['id'];
 
 
-        $epoch = $photo['dateupload']; 
-        $dt = new DateTime("@$epoch");  // convert UNIX timestamp to PHP DateTime
- 
-          try{
-            //file_put_contents($filename, file_get_contents($url));
+      for($page=$startpage; $page <= $pages; $page++){
+
+        echo "\n\n STARTPAGE ".$startpage;
+        echo "\n\n CURRENT PAGE ".$page;
+        echo "\n\n TOTAL PAGES ".$pages;
+        $counter = 0;
+        echo "\nFETCHING FOR PAGE: ".$page;
+        echo "\nBEFORE: ".$before;
+        echo "\n\nAFTERL ".$after;
+        $photos = $f->photos_search(array("max_upload_date"=>$after,"min_upload_date"=>$before,"per_page"=>"500","license"=>"1,2,3,4,5,6,7","page"=>$page,"extras"=>"url_o,owner_name, license, date_upload"));
+
+
+        $i = 1;
+
+        foreach ($photos['photo'] as $photo) {
+          if($page > $startpage){
+            $interrupt = false;
+          }
+
+          if($interrupt === true){
+            echo "\n\n************ INTERRUPT ***************\n";
+          }
+          echo "\n\n PAGE ".$page; 
+          echo "\n\n OF INTERVAL: ".$x;
+          $duplicate = false;
+
+          if($interrupt === false || ($interrupt === true && $lines < $i)){
+      
+
+            $url = $photo['url_o'];
+            $namefile = explode("/",$url);
+            $filename = $dir."/".$namefile[4];
+            $crediturl = "https://flickr.com/photos/".$photo['owner']."/".$photo['id'];
+
+
+            $epoch = $photo['dateupload']; 
+            $dt = new DateTime("@$epoch");  // convert UNIX timestamp to PHP DateTime
+            echo "\n\n DOWNLOADED: ".$dt->format('Y-m-d H:i:sP');
+     
+              try{
+                //file_put_contents($filename, file_get_contents($url));
+
+                
+
+                  if(file_exists($filename) && ($lines + 1) != $i){
+                    echo "\n\n DUPLICATE\n";
+                    $duplicate = true;
+                  } else{
+                    $duplicate = false;
+                    echo "\n\n LINES ".$lines;
+                    echo "\n\n i ".$i;
+                    echo "\n\n FILE DOESNT EXIST!!";
+
+                  
+                  $ch = curl_init($url);
+                  curl_setopt($ch, CURLOPT_HEADER, 0);
+                  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                  curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
+                  $rawdata=curl_exec ($ch);
+                  curl_close ($ch);
+
+                  $fp = fopen($filename,'w');
+                  fwrite($fp, $rawdata); 
+                  fclose($fp);
+
+               
 
             
+               
+               //exec("wget ".$url." -O ".$filename);
 
-         
+                $license = $photo['license'];
+
+                //echo "\n\n LICENSE: ".$license;
+
+                switch ($photo['license']) {
+                  case "1":
+                  $license = $license1;
+                  break;
+                  case "2":
+                  $license = $license2;
+                  break;
+                  case "3":
+                  $license = $license3;
+                  break;
+                  case "4":
+                  $license = $license4;
+                  break;
+                  case "5":
+                  $license = $license5;
+                  break;
+                  case "6":
+                  $license = $license6;
+                  break;
+                  case "7":
+                  $license = $license7;
+                  break;
+                  default:
+                  echo $license;
+                }
+
+                try{
+                  $photograph = file_get_contents($filename);
+                }catch (Exception $e){
+                  echo "\n\n GET FILE ERROR: ".$e;
+                }
+
+
+
+                $authorname = str_replace(array("\n", "\r"), ' ', $photo['ownername']);
+                $title = (empty($photo['title'])) ? "No Title" : str_replace(array("\n", "\r"), ' ', $photo['title']);
+}
+              }catch (Exception $e){
+                echo "\n\n DOWNLOAD ERROR: ".$e."\n\n";
+              }
+            
+              $sqlite_timestamp = date(DATE_RFC3339);
+
+              //Check for empty fields
+              if(empty($license) || empty($photograph) || empty($title)
+                || empty($authorname) || empty($url) || empty($crediturl) || ($duplicate === true)){
+
+                echo "\n\n ERROR!!!";
+                if($duplicate === true){
+                echo "\n\n DUBBB *****************************";
+                $duperror = $namefile[4].$del.$filename.$del.$crediturl.$del.$epoch.$del.$dt->format('Y-m-d H:i:s').$del.$date.$del.$x.$del.$page.$del.$i.$before.$del.$after."\n";
+                fwrite($duplicates, $errorname);
+              }else{
+
+                $errorname = (empty($photos))? "No RESPONSE\n" : "";
+       
+                $errorname = $license.$del.$title.$del.$authorname.$del.$url.$del.$sqlite_timestamp.$del.$namefile[4].$del.$filename.$del.$crediturl.$del.$epoch.$del.$dt->format('Y-m-d H:i:s').$del.$date.$del.$x.$del.$page.$del.$i.$del;
+                $errorname.= (empty($photograph)) ? " no PHOTO" : ""; 
+                $errorname.= (empty($title)) ? " no TITLE" : ""; 
+                $errorname.=(empty($url)) ? "NO URL" : "";
+                $errorname.="\n";
               
-              $ch = curl_init($url);
-              curl_setopt($ch, CURLOPT_HEADER, 0);
-              curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-              curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
-              $rawdata=curl_exec ($ch);
-              curl_close ($ch);
+                fwrite($errorfile, $errorname);
+              }
+                $errorcount++; 
 
-              $fp = fopen($filename,'w');
-              fwrite($fp, $rawdata); 
-              fclose($fp);
-
-           
-
+            }else{
         
-           
-           //exec("wget ".$url." -O ".$filename);
-
-            $license = $photo['license'];
-
-            //echo "\n\n LICENSE: ".$license;
-
-            switch ($photo['license']) {
-              case "1":
-              $license = $license1;
-              break;
-              case "2":
-              $license = $license2;
-              break;
-              case "3":
-              $license = $license3;
-              break;
-              case "4":
-              $license = $license4;
-              break;
-              case "5":
-              $license = $license5;
-              break;
-              case "6":
-              $license = $license6;
-              break;
-              case "7":
-              $license = $license7;
-              break;
-              default:
-              echo $license;
-            }
-
-            try{
-              $photograph = file_get_contents($filename);
-            }catch (Exception $e){
-              echo "\n\n GET FILE ERROR: ".$e;
-            }
-
-
-
-            $authorname = str_replace(array("\n", "\r"), ' ', $photo['ownername']);
-            $title = (empty($photo['title'])) ? "No Title" : str_replace(array("\n", "\r"), ' ', $photo['title']);
-
-          }catch (Exception $e){
-            echo "\n\n DOWNLOAD ERROR: ".$e."\n\n";
+              $str = $license.$del.$title.$del.$authorname.$del.$url.$del.$sqlite_timestamp.$del.$namefile[4].$del.$filename.$del.$crediturl.$del.$epoch.$del.$dt->format('Y-m-d H:i:s').$del.$date.$del.$x.$del.$page.$del.$i."\n";
+              fwrite($myfile, $str);
+              $interrupt = false;
           }
-          $sqlite_timestamp = date(DATE_RFC3339);
-          //Check for empty fields
-          if(empty($license) || empty($photograph) || empty($title)
-            || empty($authorname) || empty($url) || empty($crediturl)){
+          $counter = $i; 
+          echo "\n ** COUNTER: ".$counter;    
+          $counter++;
+          
+      }// close the if interrupt statement
 
-            $errorname = (empty($photos))? "No RESPONSE\n" : "";
-   
-            $errorname = $license.$del.$title.$del.$authorname.$del.$url.$del.$sqlite_timestamp.$del.$namefile[4].$del.$filename.$del.$crediturl.$del.$epoch.$del.$dt->format('Y-m-d H:i:s').$del;
-            $errorname.= (empty($photograph)) ? " no PHOTO" : ""; 
-            $errorname.= (empty($title)) ? " no TITLE" : ""; 
-            $errorname.=(empty($url)) ? "NO URL" : "";
-            $errorname.="\n";
-            fwrite($errorfile, $errorname);
-            $errorcount++; 
-        }else{
-    
-          $str = $license.$del.$title.$del.$authorname.$del.$url.$del.$sqlite_timestamp.$del.$namefile[4].$del.$filename.$del.$crediturl.$del.$epoch.$del.$dt->format('Y-m-d H:i:s')."\n";
-          fwrite($myfile, $str);
+      $i++;
+        } // close foreach photo
       }
-      $counter = $i; 
-      echo "\n ** COUNTER: ".$counter;    
-      $counter++;
-      
-  }// close the if interrupt statement
-
-  $i++;
-    } // close foreach photo
-  }
-
-
+      //$beforet = $aftert;
+    } // close for loop
+} // close interrupt 
   } catch (Exception $e){
     echo "\n Connection error\n";
   }
@@ -238,4 +319,39 @@ mkdir($dir);
 
   fclose($myfile);
   fclose($errorfile);
+
+  function read_last_line ($file_path){
+
+
+
+$line = '';
+
+$f = fopen($file_path, 'r');
+$cursor = -1;
+
+fseek($f, $cursor, SEEK_END);
+$char = fgetc($f);
+
+/**
+* Trim trailing newline chars of the file
+*/
+while ($char === "\n" || $char === "\r") {
+    fseek($f, $cursor--, SEEK_END);
+    $char = fgetc($f);
+}
+
+/**
+* Read until the start of file or first newline char
+*/
+while ($char !== false && $char !== "\n" && $char !== "\r") {
+    /**
+     * 179pend the new char
+     */
+    $line = $char . $line;
+    fseek($f, $cursor--, SEEK_END);
+    $char = fgetc($f);
+}
+
+return $line;
+}
 ?>
--- downloader/images-flickr/readflickr.php
+++ downloader/images-flickr/readflickr.php
@@ -9,13 +9,14 @@ foreach ($argv as $arg) {
   if(count($e)==2){
     $_GET[$e[0]]=$e[1];
   }else
-    $_GET[]=$e[0];        
-  }
+  $_GET[]=$e[0];        
+}
 
-  $filename = $_GET[1];
+// Get filename from commandline
+$filename = $_GET[1];
+
+
 // Database stats
-
-
 $counter = 0;
 $servername = "localhost";
 $username = "anna";
@@ -25,119 +26,129 @@ $dbname = "hashes";
 
 $myfile = fopen($filename, "r") or die("Unable to open file!");
 
-
-
-// Create connection
+// Create DB connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 } 
 
 $f = new phpFlickr("dd5266efb4a0e67238c32f8b8cfa2f92");
 if ($myfile) {
-    while (($line = fgets($myfile)) !== false) {
-        $counter++;
-        $delim = '\',\'';
-        //$dir = str_replace("flickrdownload.txt", "pics", $filename);
+  while (($line = fgets($myfile)) !== false) {
+    $counter++;
+    $delim = '\',\'';
+    $imageinfo = explode($delim,$line);
+    print_r($imageinfo);
 
-        
-        $imageinfo = explode($delim,$line);
-        print_r($imageinfo);
 
-        $license = $imageinfo[0];
-        $title = $imageinfo[1];
-        $author = $imageinfo[2];
-        $imageurl = $imageinfo[3];
-        $imagefile = $imageinfo[6];
-        $name = $imageinfo[5];
-        $url = $imageinfo[7];
-        $uploaddateunix = $imageinfo[8];
-        $uploaddate = $imageinfo[9];
+    //Get file contents
+    $license = $imageinfo[0];
+    $title = $imageinfo[1];
+    $author = $imageinfo[2];
+    $imageurl = $imageinfo[3];
+    $imagefile = $imageinfo[6];
+    $name = $imageinfo[5];
+    $url = $imageinfo[7];
+    $uploaddateunix = $imageinfo[8];
+    $uploaddate = $imageinfo[9];
+    $dir = explode('/',$imagefile);
+    $dir = $dir[0].'/thumbs';
+    mkdir($dir);
+    echo "\n\n DIR: ".$dir;
 
-        if(strpos($filename, 'error') === false){
-          //$dir = str_replace("flickrdownload.txt", "pics", $filename);
-          //echo "\n\nDIR: ".$dir;
-          
-        }else{
-         
+    if(strpos($filename, 'error') === false){
 
-          $dlAttempt = 0;
 
-            while ($dlAttempt < 5) {
-              echo "\n\n DL ATTEMPTS: ".$dlAttempt;
-              $ch = curl_init($imageurl);
-              curl_setopt($ch, CURLOPT_HEADER, 0);
-              curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-              curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
-              $rawdata=curl_exec ($ch);
-              curl_close ($ch);
+    }else{
 
-              $fp = fopen($imagefile,'w');
-              fwrite($fp, $rawdata); 
-              fclose($fp);
+      // If it is error file, download the image again.
+      $dlAttempt = 0;
+      while ($dlAttempt < 5) {
+        echo "\n\n DL ATTEMPTS: ".$dlAttempt;
+        $ch = curl_init($imageurl);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
+        $rawdata=curl_exec ($ch);
+        curl_close ($ch);
 
-              if(empty(file_get_contents($imagefile))){
-                $dlAttempt++;
-              } else {
-                $dlAttempt=5;
-              }
+        $fp = fopen($imagefile,'w');
+        fwrite($fp, $rawdata); 
+        fclose($fp);
 
-            }  
-
+        if(empty(file_get_contents($imagefile))){
+          $dlAttempt++;
+        } else {
+          $dlAttempt=5;
         }
-             
-        try{
-          $hash = exec('./phash '.$imagefile);
-          }catch (Exception $e) {
-              echo "\n\n HASH ERROR: ".$e."\n\n";
-          }
-          try{
-              $mhash = exec('./phashmh '.$imagefile);
-          }
-          catch (Exception $e){
-              echo "\n\n MPH HASH EXCEPTION!!! ".$e."\n\n";
-          }
+
+      }  
+
+    }
+
+    try{
+      $hash = exec('./phash '.$imagefile);
+    }catch (Exception $e) {
+      echo "\n\n HASH ERROR: ".$e."\n\n";
+    }
+    try{
+      $mhash = exec('./phashmh '.$imagefile);
+    }
+    catch (Exception $e){
+      echo "\n\n MPH HASH EXCEPTION!!! ".$e."\n\n";
+    }
 
             // Resize the image to thumbnail after hashing
-            try{
-                $str = 'convert '.$imagefile.' -resize 200 '.$imagefile;
-                $thumb = exec($str);
-            }
-            catch (Exception $e){
-                echo "\nRESIZE ERROR\n".$imagefile;
+    try{
+      $str = 'convert '.escapeshellarg($imagefile).' -resize 200 '.escapeshellarg($dir."/".$name);
+      $thumb = exec($str);
+    }
+    catch (Exception $e){
+      echo "\nRESIZE ERROR\n".$imagefile;
 
-            }
+    }
+    if(empty($hash) || empty($imageinfo) || empty($mhash) || empty(file_get_contents($imagefile))){
+      echo "\n\n EMPTY!! \n";
 
-           // $hash = (empty($hash)) ? 14620491339638543539 : $hash;
-           // $mhash = (empty($mhash)) ? "0c8e470ba1c0e874792da25f0561e1b9904d65c4f26d6ed316070a6ccb8a6466e9b232832d88eda61f2e230ccbc534ae5c5c1fc2f24964e2f8c29c8bc71e5b288fc7e3f1e8fc7e47" : $mhash;
-       
-      if(empty($hash) || empty($imageinfo) || empty($mhash) || empty(file_get_contents($imagefile))){
-        echo "\n\n EMPTY!! \n";
+    }else {
 
-                }else {
-                    try{
-                      $author = mysqli_real_escape_string($conn, $author);
-                      $title = mysqli_real_escape_string($conn, $title);
+      try{
+        // See if that name exists.
+        $sql = "select * from hashes.IMG33 where name='{$name}';";
+        $resulttest = $conn->query($sql);
+
+        if (!mysqli_query($conn,$sql)) {
+          die('Error: ' . mysqli_error($conn));
+        }
+        $rows = $resulttest->num_rows;
+        if ($rows > 0) {
+          echo "\n\n\nFILE EXISTS IN THE DATABASE!!! ";
+        }else{
+
+          //If that file doesn't exist, insert into database.
+          $author = mysqli_real_escape_string($conn, $author);
+          $title = mysqli_real_escape_string($conn, $title);
 
                         // MYSql Insert Statement
-                        $sql = "INSERT INTO IMG33(phash,mhash,name,title, directory,author, license, url, imageurl, source, dateuploaded, dateuploadu) 
-                                VALUES('$hash','$mhash','$name', '$title','$imagefile', '$author','$license','$url', '$imageurl', 'Flickr', '$uploaddate', '$uploaddateunix')";
-                        if ($conn->query($sql) === TRUE) {
-                            echo "New records created successfully";
-                        } else {
-                            echo "Error: <br>" . $conn->error;
-                        }
-                    }
-                    catch (Exception $e){
-                        echo "\n\nDB ERROR!! ".$e;
-                  
-                    }
-                }
+          $sql = "INSERT INTO IMG33(phash,mhash,name,title, directory,author, license, url, imageurl, source, dateuploaded, dateuploadu) 
+          VALUES('$hash','$mhash','$name', '$title','$imagefile', '$author','$license','$url', '$imageurl', 'Flickr', '$uploaddate', '$uploaddateunix')";
+          if ($conn->query($sql) === TRUE) {
+            echo "New records created successfully";
+          } else {
+            echo "Error: <br>" . $conn->error;
+          }
+        }
+      }
+      catch (Exception $e){
+        echo "\n\nDB ERROR!! ".$e;
 
-}
-    echo "COUNTER: \n".$counter."\n";
-    fclose($myfile);
+      }
+    }
+
+  }
+  echo "COUNTER: \n".$counter."\n";
+  fclose($myfile);
 } else {
-    echo 'error opening the file';
+  echo 'error opening the file';
 } 
